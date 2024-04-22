8f5d3c6b2e0f68949235c95fab12ecd75369a7ae
JEFFERSON <loartejefferson5@gmail.com>
2024-04-21 21:06:59 -0500

    segundo commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function abrirTarjeta() {
+    document.getElementById("sobre").style.display = "none";
+    document.getElementById("videoContainer").style.display = "block";
+    document.getElementById("video").play(); // Reproducir el video automáticamente al abrir la tarjeta
+}
